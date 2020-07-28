d281c861ac54f62d672a8871d281f5e02daaa0b3
samuelematias <hi@samuelematias.com>
2020-07-28 14:09:21 -0300

    feat(package-test): write tests

--- packages/bmi/test/bmi_test.dart
+++ packages/bmi/test/bmi_test.dart
@@ -1,5 +1,22 @@
+import 'package:bmi/bmi.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('test', () {});
+  group('calculateBMI', () {
+    test('return right value = 61.7', () {
+      final calculate = calculateBMI(200, 1.80);
+
+      expect(formattedBmi(calculate), "61.7");
+    });
+
+    test('return empty string', () {
+      try {
+        calculateBMI(200, double.tryParse("1,80"));
+      } on NoSuchMethodError catch (e) {
+        expect(e.toString(),
+            contains("NoSuchMethodError: The method '*' was called on null"));
+        return;
+      }
+    });
+  });
 }
