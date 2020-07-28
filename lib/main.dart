a978560223b4734d8ece88ddc06c442d9cdc243f
samuelematias <hi@samuelematias.com>
2020-07-28 11:21:00 -0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bmi_calculator_app_flutter/bmi_calculation_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: BmiCalculationPage(),
+    );
+  }
+}
