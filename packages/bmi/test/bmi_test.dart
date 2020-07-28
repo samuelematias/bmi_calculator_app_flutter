7e7371dab0ff7654241747295e9d26d632bc21d2
samuelematias <hi@samuelematias.com>
2020-07-28 16:14:32 -0300

    feat(tests): update tests

--- packages/bmi/test/bmi_test.dart
+++ packages/bmi/test/bmi_test.dart
@@ -6,13 +6,13 @@ void main() {
     test('return right value = 61.7', () {
       final calculate = calculateBMI(200, 1.80);
 
-      expect(formattedBmi(calculate), "61.7");
+      expect(formattedBmi(calculate), '61.7');
     });
 
     test('return empty string', () {
       try {
-        calculateBMI(200, double.tryParse("1,80"));
-      } on NoSuchMethodError catch (e) {
+        calculateBMI(200, double.tryParse('1,80'));
+      } catch (e) {
         expect(e.toString(),
             contains("NoSuchMethodError: The method '*' was called on null"));
         return;
